30fcc116b683f3c2f39b7bca48e47cebb8db3f56
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-05-06 02:10:37 +0300

    `feat(currency): add CreateCurrencyRequest DTO`
    • Introduce DTO for creating new currency entities.
    • Validate fields with constraints (e.g., code, name, country).
    • Ensure decimal field has range validation (0-10).

---
+++ backend/src/main/java/com/alligator/market/backend/currency/dto/CreateCurrencyRequest.java
@@ -0,0 +1,25 @@
+package com.alligator.market.backend.currency.dto;
+
+import jakarta.validation.constraints.*;
+
+/* DTO для добавления валюты. */
+public record CreateCurrencyRequest(
+
+        @NotBlank
+        @Pattern(regexp = "^[A-Z]{3}$")
+        String code,
+
+        @NotBlank
+        @Size(max = 50)
+        String name,
+
+        @NotBlank
+        @Size(max = 100)
+        String country,
+
+        @NotNull
+        @Min(0) @Max(10)
+        Integer decimal
+) {
+}
+
