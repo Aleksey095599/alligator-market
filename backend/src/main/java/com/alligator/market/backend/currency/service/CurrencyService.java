b5c671984d7ddf1ac94a6965ea030db4aa37dde3
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-05-07 00:39:28 +0300

    feat(currency-service): add interface for currency operations
    • Define an interface for currency operations in CurrencyService
    • Prepare for modular and standardized currency handling

--- backend/src/main/java/com/alligator/market/backend/currency/service/CurrencyService.java
+++ backend/src/main/java/com/alligator/market/backend/currency/service/CurrencyService.java
@@ -3,6 +3,9 @@ package com.alligator.market.backend.currency.service;
 import com.alligator.market.backend.currency.dto.CreateCurrencyRequest;
 import com.alligator.market.backend.currency.entity.Currency;
 
+/* Интерфейс сервиса для операций с валютами. */
 public interface CurrencyService {
+
     Currency create(CreateCurrencyRequest dto);
+
 }
