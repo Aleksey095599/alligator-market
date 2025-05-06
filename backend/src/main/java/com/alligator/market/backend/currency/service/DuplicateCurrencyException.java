34332574a30396bc4e4e0e355df0c701d6199a0d
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-05-07 01:17:15 +0300

    ``` refactor(currency): update exception comment

--- backend/src/main/java/com/alligator/market/backend/currency/service/DuplicateCurrencyException.java
+++ backend/src/main/java/com/alligator/market/backend/currency/service/DuplicateCurrencyException.java
@@ -1,6 +1,6 @@
 package com.alligator.market.backend.currency.service;
 
-/* Исключение для дублирующейся валюты по коду. */
+/* Исключение для дублирующейся валюты. */
 public class DuplicateCurrencyException extends RuntimeException {
     public DuplicateCurrencyException(String field, String value) {
         super("Currency with " + field + " '" + value + "' already exists");
