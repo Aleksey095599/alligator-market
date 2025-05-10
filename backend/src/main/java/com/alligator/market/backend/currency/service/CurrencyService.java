75afebfaeb12fce2edfe504b8226accb0d3ae330
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-05-10 03:47:32 +0300

    refactor(currency): update createCurrency return type

--- backend/src/main/java/com/alligator/market/backend/currency/service/CurrencyService.java
+++ backend/src/main/java/com/alligator/market/backend/currency/service/CurrencyService.java
@@ -1,11 +1,10 @@
 package com.alligator.market.backend.currency.service;
 
 import com.alligator.market.backend.currency.dto.CreateCurrencyRequest;
-import com.alligator.market.backend.currency.entity.Currency;
 
 /* Интерфейс сервиса для операций с валютами. */
 public interface CurrencyService {
 
-    Currency createCurrency(CreateCurrencyRequest dto);
+    String createCurrency(CreateCurrencyRequest dto);
 
 }
