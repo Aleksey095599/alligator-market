320ca55b9a6fb7cc73c9b8968e66629981922f68
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-05-11 21:12:07 +0300

    `refactor(exceptions): use String.formatted for message formatting in DuplicateCurrencyException`

--- backend/src/main/java/com/alligator/market/backend/currency/service/exceptions/DuplicateCurrencyException.java
+++ backend/src/main/java/com/alligator/market/backend/currency/service/exceptions/DuplicateCurrencyException.java
@@ -1,8 +1,7 @@
 package com.alligator.market.backend.currency.service.exceptions;
 
-/* Исключение для дублирующейся валюты. */
 public class DuplicateCurrencyException extends RuntimeException {
     public DuplicateCurrencyException(String field, String value) {
-        super("Currency with " + field + " '" + value + "' already exists");
+        super("Currency with %s '%s' already exists".formatted(field, value));
     }
 }
