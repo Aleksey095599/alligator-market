445ad29bf77a5e1f9f8c3ae23665510399f93f26
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-05-25 19:57:09 +0300

    feat(currency): add method to fetch all currencies

--- backend/src/main/java/com/alligator/market/backend/currency/service/CurrencyService.java
+++ backend/src/main/java/com/alligator/market/backend/currency/service/CurrencyService.java
@@ -2,10 +2,13 @@ package com.alligator.market.backend.currency.service;
 
 import com.alligator.market.backend.currency.dto.CurrencyDto;
 
+import java.util.List;
+
 /* Интерфейс сервиса для операций с валютами. */
 public interface CurrencyService {
 
     String createCurrency(CurrencyDto dto);
-
     void deleteCurrency(String code);
+
+    List<CurrencyDto> findAll();
 }
