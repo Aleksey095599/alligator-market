e0ce7f48b2a24ae387c5942a47673bb48925c053
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-05-27 21:50:03 +0300

    `feat(currency): add route for CurrencyAdminComponent`

--- frontend/src/app/currency/currency-routing.module.ts
+++ frontend/src/app/currency/currency-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/currency-admin/currency-admin.component')
+        .then(c => c.CurrencyAdminComponent)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
