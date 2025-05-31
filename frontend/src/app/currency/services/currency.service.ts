648a310098e63ec10b6df6b9fbbe7bec9caf7823
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-05-31 19:46:52 +0300

    feat(currency): add update method to currency service

--- frontend/src/app/currency/services/currency.service.ts
+++ frontend/src/app/currency/services/currency.service.ts
@@ -3,6 +3,7 @@ import {CurrencyDto} from '../models/currency.model';
 import {map, Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {ApiResponse} from '../../shared/api-response.model';
+import {CurrencyUpdateDto} from "../models/currency-update.model";
 
 /* Сервис для взаимодействия с API по работе с валютами */
 @Injectable({
@@ -34,6 +35,13 @@ export class CurrencyService {
   delete(code: string): Observable<void> {
     return this.http
       .delete<ApiResponse<void>>(`${this.baseUrl}/${code}`)
+      .pipe(map(res => res.data));
+  }
+
+  /* Обновить валюту по коду */
+  update(code: string, dto: CurrencyUpdateDto): Observable<void> {
+    return this.http
+      .put<ApiResponse<void>>(`${this.baseUrl}/${code}`, dto)
       .pipe(map(res => res.data));
   }
 
