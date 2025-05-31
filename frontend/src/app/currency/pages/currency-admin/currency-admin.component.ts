e06349d2d1f5895206d1985ca58298ddf6c6ae60
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-05-31 17:50:33 +0300

    `feat(currency-admin): add utility method to refresh list`

--- frontend/src/app/currency/pages/currency-admin/currency-admin.component.ts
+++ frontend/src/app/currency/pages/currency-admin/currency-admin.component.ts
@@ -132,6 +132,9 @@ export class CurrencyAdminComponent implements OnInit {
     });
   }
 
+  //========================
+  // Вспомогательные утилиты
+  //========================
   /* утилита: перезагрузить список */
   private refresh(): void {
     this.service.list().subscribe({
