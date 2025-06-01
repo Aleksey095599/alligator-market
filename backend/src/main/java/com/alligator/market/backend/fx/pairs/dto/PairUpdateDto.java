ecaa6bebc402bea621e3c6682ee79c3ec9756e74
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-06-01 21:41:19 +0300

    refactor(dto): remove redundant comment in PairUpdateDto

--- backend/src/main/java/com/alligator/market/backend/fx/pairs/dto/PairUpdateDto.java
+++ backend/src/main/java/com/alligator/market/backend/fx/pairs/dto/PairUpdateDto.java
@@ -1,11 +1,12 @@
 package com.alligator.market.backend.fx.pairs.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 /* DTO для обновления валютной пары. */
 public record PairUpdateDto(
 
-        /* Кол-во знаков после запятой для курса */
         @NotNull
         @Min(0) @Max(10)
         Integer decimal
