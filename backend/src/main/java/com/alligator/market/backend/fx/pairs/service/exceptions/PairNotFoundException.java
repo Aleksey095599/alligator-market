e518b0d75fb0afccafebf3f21886626031aadec3
Aleksey095599 <alexeybaranov86@gmail.com>
2025-06-01 21:51:45 +0300

    feat(backend): add pair module

---
+++ backend/src/main/java/com/alligator/market/backend/fx/pairs/service/exceptions/PairNotFoundException.java
@@ -0,0 +1,9 @@
+package com.alligator.market.backend.fx.pairs.service.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+
+public class PairNotFoundException extends EntityNotFoundException {
+    public PairNotFoundException(String pair) {
+        super("Pair '%s' not found".formatted(pair));
+    }
+}
---
+++ backend/src/main/java/com/alligator/market/backend/fx/pairs/web/PairController.java
@@ -0,0 +1,71 @@
+package com.alligator.market.backend.fx.pairs.web;
+
+import com.alligator.market.backend.common.web.dto.ApiResponse;
+import com.alligator.market.backend.common.web.util.ResponseEntityFactory;
+import com.alligator.market.backend.fx.pairs.dto.PairDto;
+import com.alligator.market.backend.fx.pairs.dto.PairUpdateDto;
+import com.alligator.market.backend.fx.pairs.service.PairService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+/* REST-контроллер для операций с валютными парами. */
+@RestController
+@RequestMapping("/api/v1/pairs")
+@RequiredArgsConstructor
+@Slf4j
+public class PairController {
+
+    private final PairService service;
+
+    //=====================
+    // Создать новую пару
+    //=====================
+    @PostMapping
+    public ResponseEntity<ApiResponse<String>> create(@RequestBody @Valid PairDto dto) {
+
+        String pair = service.createPair(dto);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{pair}")
+                .buildAndExpand(pair)
+                .toUri();
+        return ResponseEntityFactory.created(location, pair);
+    }
+
+    //=================
+    // Обновить пару
+    //=================
+    @PutMapping("/{pair}")
+    public ResponseEntity<ApiResponse<Void>> update(
+            @PathVariable String pair,
+            @RequestBody @Valid PairUpdateDto dto) {
+        service.updatePair(pair, dto);
+        return ResponseEntityFactory.ok(null);
+    }
+
+    //================
+    // Удалить пару
+    //================
+    @DeleteMapping("/{pair}")
+    public ResponseEntity<ApiResponse<Void>> delete(@PathVariable String pair) {
+
+        service.deletePair(pair);
+        return ResponseEntityFactory.ok(null);
+    }
+
+    //===================
+    // Вернуть все пары
+    //===================
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<PairDto>>> getAll() {
+
+        return ResponseEntityFactory.ok(service.findAll());
+    }
+}
