b17df0aa0f0ada01d348bb0a3e15ba328193639e
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-06-02 23:04:13 +0300

    refactor(exceptions): unify comment style in PairCurrencyNotFoundException

--- backend/src/main/java/com/alligator/market/backend/fx/pairs/service/exceptions/PairCurrencyNotFoundException.java
+++ backend/src/main/java/com/alligator/market/backend/fx/pairs/service/exceptions/PairCurrencyNotFoundException.java
@@ -1,6 +1,6 @@
 package com.alligator.market.backend.fx.pairs.service.exceptions;
 
-/** Валюта для валютной пары не найдена. */
+/* Валюта для валютной пары не найдена. */
 public class PairCurrencyNotFoundException extends RuntimeException {
     public PairCurrencyNotFoundException(String code) {
         super("Currency '%s' not found".formatted(code));
