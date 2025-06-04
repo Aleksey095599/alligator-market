c5a4fba6eb2658287826c8bd42e3849db9119eb4
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-06-04 21:38:39 +0300

    `feat(core): add initial App class with main method`

---
+++ core/src/main/java/com/alligator/core/App.java
@@ -0,0 +1,13 @@
+package com.alligator.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
