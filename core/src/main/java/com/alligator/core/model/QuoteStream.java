12fe7d533e3fa5ccb66994ed1dd15985d00121b8
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-06-08 12:51:49 +0300

    `chore(core): remove QuoteStream interface`

--- core/src/main/java/com/alligator/core/model/QuoteStream.java
+++
@@ -1,8 +0,0 @@
-package com.alligator.core.model;
-
-import reactor.core.publisher.Flux;
-
-/* Входной порт: подписка на поток котировок. */
-public interface  QuoteStream {
-    Flux<CurrencyQuote> streamAll();
-}
