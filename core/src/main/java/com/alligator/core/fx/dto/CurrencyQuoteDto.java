25afb2cc6990962e82aaf67f4ab54d1eeaca3484
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-06-08 14:34:21 +0300

    docs(core): improve CurrencyQuoteDto comment description

--- core/src/main/java/com/alligator/core/fx/dto/CurrencyQuoteDto.java
+++ core/src/main/java/com/alligator/core/fx/dto/CurrencyQuoteDto.java
@@ -3,7 +3,7 @@ package com.alligator.core.fx.dto;
 import java.math.BigDecimal;
 import java.time.Instant;
 
-/* DTO котировки для отправки на клиент. */
+/** DTO котировки для валютной пары (например, для отправки с backend на frontend). */
 public record CurrencyQuoteDto(
         Long pairId,
         BigDecimal bid,
