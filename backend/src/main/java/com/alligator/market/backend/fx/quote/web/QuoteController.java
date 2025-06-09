892d146a6e9459123673d02078682da6218c6a98
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-06-09 21:14:58 +0300

    style(quote): reformat comments for consistency

--- backend/src/main/java/com/alligator/market/backend/fx/quote/web/QuoteController.java
+++ backend/src/main/java/com/alligator/market/backend/fx/quote/web/QuoteController.java
@@ -32,10 +32,7 @@ public class QuoteController {
     //-----------------------
     // Вспомогательные методы
     //-----------------------
-
-    /**
-     * Вспомогательный метод конвертации доменной модели котировки в DTO для передачи на frontend.
-     */
+    /* Вспомогательный метод конвертации доменной модели котировки в DTO для передачи на frontend. */
     private CurrencyQuoteDto toDto(CurrencyQuote q) {
         return new CurrencyQuoteDto(q.pairId(), q.bid(), q.ask(), q.ts());
     }
