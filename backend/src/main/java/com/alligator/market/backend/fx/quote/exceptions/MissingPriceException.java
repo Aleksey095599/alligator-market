fe1890d57cc5c553ae866f22ef20e9627855c1a8
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-06-17 07:10:42 +0300

    deleted

--- backend/src/main/java/com/alligator/market/backend/fx/quote/exceptions/MissingPriceException.java
+++
@@ -1,8 +0,0 @@
-package com.alligator.market.backend.fx.quote.exceptions;
-
-/* Отсутствует цена в ответе провайдера котировок. */
-public class MissingPriceException extends RuntimeException {
-    public MissingPriceException(String symbol) {
-        super("Price missing for symbol '%s'".formatted(symbol));
-    }
-}
