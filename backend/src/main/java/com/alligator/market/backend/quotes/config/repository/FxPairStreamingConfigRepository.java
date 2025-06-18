3e7c72813787e289c7dded130dd24d24e25556cb
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-06-18 22:52:31 +0300

    feat(quotes): add FxPairStreamingConfigRepository

---
+++ backend/src/main/java/com/alligator/market/backend/quotes/config/repository/FxPairStreamingConfigRepository.java
@@ -0,0 +1,14 @@
+package com.alligator.market.backend.quotes.config.repository;
+
+import com.alligator.market.backend.quotes.config.entity.FxPairStreamingConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * Репозиторий таблицы настроек стрима котировок для заданной валютной пары.
+ */
+public interface FxPairStreamingConfigRepository extends JpaRepository<FxPairStreamingConfig, Long> {
+
+    Optional<FxPairStreamingConfig> findByPair_PairAndProvider(String pair, String provider);
+}
