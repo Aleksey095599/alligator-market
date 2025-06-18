47361b2b63580b7ff9b31ae2bb1a3ce8c30c94f8
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-06-18 22:52:52 +0300

    feat(quotes): add FxPairStreamingConfigServiceImpl

---
+++ backend/src/main/java/com/alligator/market/backend/quotes/config/service/FxPairStreamingConfigServiceImpl.java
@@ -0,0 +1,9 @@
+package com.alligator.market.backend.quotes.config.service;
+
+/**
+ * Реализация сервиса настроек стрима котировок для заданной валютной пары.
+ */
+public class FxPairStreamingConfigServiceImpl {
+
+
+}
