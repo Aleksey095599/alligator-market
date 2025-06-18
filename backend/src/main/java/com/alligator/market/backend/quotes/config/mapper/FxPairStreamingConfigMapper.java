774510a26cee72362414bd75cb869b318b1a9123
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-06-18 22:52:18 +0300

    feat(quotes): add FxPairStreamingConfigMapper

---
+++ backend/src/main/java/com/alligator/market/backend/quotes/config/mapper/FxPairStreamingConfigMapper.java
@@ -0,0 +1,33 @@
+package com.alligator.market.backend.quotes.config.mapper;
+
+import com.alligator.market.backend.pairs.entity.Pair;
+import com.alligator.market.backend.quotes.config.entity.FxPairStreamingConfig;
+import org.springframework.stereotype.Component;
+
+/**
+ * Маппер между доменной моделью и соответствующим entity.
+ * Импорт доменной модели в backend обусловлен использованием этой модели в нескольких слоях.
+ */
+@Component
+public class FxPairStreamingConfigMapper {
+
+    public com.alligator.market.domain.model.FxPairStreamingConfig toDomain(FxPairStreamingConfig entity) {
+        return new com.alligator.market.domain.model.FxPairStreamingConfig(
+                entity.getPair().getPair(),
+                entity.getProvider(),
+                entity.getPriority(),
+                entity.getRefreshMs(),
+                entity.isEnabled()
+        );
+    }
+
+    public FxPairStreamingConfig toEntity(com.alligator.market.domain.model.FxPairStreamingConfig cfg, Pair pair) {
+        FxPairStreamingConfig entity = new FxPairStreamingConfig();
+        entity.setPair(pair);
+        entity.setProvider(cfg.provider());
+        entity.setPriority(cfg.priority());
+        entity.setRefreshMs(cfg.refreshMs());
+        entity.setEnabled(cfg.enabled());
+        return entity;
+    }
+}
