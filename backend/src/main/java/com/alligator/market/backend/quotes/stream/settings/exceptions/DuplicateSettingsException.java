169052a12f2595dd94831af83fec20d8e7d22381
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-06-19 23:57:56 +0300

    refactor(quotes): update exception messages for consistency

--- backend/src/main/java/com/alligator/market/backend/quotes/stream/settings/exceptions/DuplicateSettingsException.java
+++ backend/src/main/java/com/alligator/market/backend/quotes/stream/settings/exceptions/DuplicateSettingsException.java
@@ -5,6 +5,6 @@ package com.alligator.market.backend.quotes.stream.settings.exceptions;
  */
 public class DuplicateSettingsException extends RuntimeException {
     public DuplicateSettingsException(String pair, String provider, String mode) {
-        super("Streaming config for pair '%s', provider '%s' and mode '%s' already exists".formatted(pair, provider, mode));
+        super("Streaming settings for pair '%s', provider '%s' and mode '%s' already exists".formatted(pair, provider, mode));
     }
 }
--- backend/src/main/java/com/alligator/market/backend/quotes/stream/settings/exceptions/SettingsNotFoundException.java
+++ backend/src/main/java/com/alligator/market/backend/quotes/stream/settings/exceptions/SettingsNotFoundException.java
@@ -6,11 +6,7 @@ import jakarta.persistence.EntityNotFoundException;
  * Очевидно из названия.
  */
 public class SettingsNotFoundException extends EntityNotFoundException {
-    public SettingsNotFoundException(String pair, String provider) {
-        super("Streaming config for pair '%s' and provider '%s' not found".formatted(pair, provider));
-    }
-
     public SettingsNotFoundException(String pair, String provider, String mode) {
-        super("Streaming config for pair '%s', provider '%s' and mode '%s' not found".formatted(pair, provider, mode));
+        super("Streaming settings for pair '%s', provider '%s' and mode '%s' not found".formatted(pair, provider, mode));
     }
 }
