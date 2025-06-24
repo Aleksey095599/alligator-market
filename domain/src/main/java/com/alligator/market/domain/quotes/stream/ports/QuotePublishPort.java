34434d3cebb0207bfd21ce3e399ad6805d249a99
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-06-24 23:17:06 +0300

    feat(quotes): add QuotePublishPort interface for quote publishing

---
+++ domain/src/main/java/com/alligator/market/domain/quotes/stream/ports/QuotePublishPort.java
@@ -0,0 +1,11 @@
+package com.alligator.market.domain.quotes.stream.ports;
+
+import com.alligator.market.domain.quotes.stream.QuoteTick;
+
+/** Выходной порт «сложить тик куда‑то». */
+public interface QuotePublishPort {
+
+    /* Публикует тик котировки в целевую систему. */
+    void publish(QuoteTick tick);
+    
+}
