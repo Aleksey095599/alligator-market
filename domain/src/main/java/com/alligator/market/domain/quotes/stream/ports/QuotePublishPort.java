088c340de15a64a915ab18683706282f31e6927e
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-06-24 23:17:49 +0300

    refactor(quotes): adjust formatting in QuotePublishPort documentation

--- domain/src/main/java/com/alligator/market/domain/quotes/stream/ports/QuotePublishPort.java
+++ domain/src/main/java/com/alligator/market/domain/quotes/stream/ports/QuotePublishPort.java
@@ -2,7 +2,9 @@ package com.alligator.market.domain.quotes.stream.ports;
 
 import com.alligator.market.domain.quotes.stream.QuoteTick;
 
-/** Выходной порт «сложить тик куда‑то». */
+/**
+ * Выходной порт «сложить тик куда‑то».
+ */
 public interface QuotePublishPort {
 
     /* Публикует тик котировки в целевую систему. */
