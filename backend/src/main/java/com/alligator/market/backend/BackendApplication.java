2cfa65a178b8d03c8ff985f458aa74662e387230
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-06-26 06:46:43 +0300

    chore(backend): remove unnecessary empty line in entry point

--- backend/src/main/java/com/alligator/market/backend/BackendApplication.java
+++ backend/src/main/java/com/alligator/market/backend/BackendApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 /**
  * Точка входа backend-приложения.
  */
-
 @SpringBootApplication
 @EnableScheduling
 public class BackendApplication {
