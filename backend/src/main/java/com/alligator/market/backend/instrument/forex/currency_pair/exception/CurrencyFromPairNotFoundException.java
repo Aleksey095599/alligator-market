40ed61ee1f8de84f27301c9650fceefb1f2db391
Алексей Б. <alexeybaranov86@gmail.com>
2025-07-07 00:21:58 +0300

    Refactor exception class name: rename `PairCurrencyNotFoundException` to `CurrencyFromPairNotFoundException` for clarity.

--- backend/src/main/java/com/alligator/market/backend/instrument/forex/currency_pair/exception/CurrencyFromPairNotFoundException.java
+++ backend/src/main/java/com/alligator/market/backend/instrument/forex/currency_pair/exception/CurrencyFromPairNotFoundException.java
@@ -3,8 +3,8 @@ package com.alligator.market.backend.instrument.forex.currency_pair.exception;
 /**
  * Не найдена одна из валют компонент для соответствующей валютной пары.
  */
-public class PairCurrencyNotFoundException extends RuntimeException {
-    public PairCurrencyNotFoundException(String code) {
+public class CurrencyFromPairNotFoundException extends RuntimeException {
+    public CurrencyFromPairNotFoundException(String code) {
         super("Currency '%s' not found".formatted(code));
     }
 }
