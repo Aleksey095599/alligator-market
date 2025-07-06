7f571927aca953cd7b1767d7645ef9fb4d3b052e
Алексей Б. <alexeybaranov86@gmail.com>
2025-07-07 00:22:56 +0300

    Add `CurrencyPairService` interface to define operations for forex currency pairs

---
+++ domain/src/main/java/com/alligator/market/domain/instrument/forex/currency_pair/CurrencyPairService.java
@@ -0,0 +1,17 @@
+package com.alligator.market.domain.instrument.forex.currency_pair;
+
+import java.util.List;
+
+/**
+ * Интерфейс сервиса для операций с валютными парами.
+ */
+public interface CurrencyPairService {
+
+    String createPair(CurrencyPair pair);
+
+    void updatePair(CurrencyPair pair);
+
+    void deletePair(String pair);
+
+    List<CurrencyPair> findAll();
+}
