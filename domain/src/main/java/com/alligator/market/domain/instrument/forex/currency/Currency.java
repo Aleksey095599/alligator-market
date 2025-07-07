e1e285cce52b1141fe91c0d45b44c3d9bc95087c
Алексей Б. <alexeybaranov86@gmail.com>
2025-07-07 09:11:53 +0300

    Remove redundant newline in `Currency` class definition

--- domain/src/main/java/com/alligator/market/domain/instrument/forex/currency/Currency.java
+++ domain/src/main/java/com/alligator/market/domain/instrument/forex/currency/Currency.java
@@ -4,6 +4,7 @@ package com.alligator.market.domain.instrument.forex.currency;
  * Доменная модель валюты.
  */
 public record Currency (
+
         String code,
         String name,
         String country,
