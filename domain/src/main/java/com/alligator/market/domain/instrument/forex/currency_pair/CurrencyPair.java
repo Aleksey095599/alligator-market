698ce23708a899cf4ff28b86a253a57e5509d4c0
Алексей Б. <alexeybaranov86@gmail.com>
2025-07-07 09:12:07 +0300

    Remove redundant newline in `CurrencyPair` class definition

--- domain/src/main/java/com/alligator/market/domain/instrument/forex/currency_pair/CurrencyPair.java
+++ domain/src/main/java/com/alligator/market/domain/instrument/forex/currency_pair/CurrencyPair.java
@@ -4,6 +4,7 @@ package com.alligator.market.domain.instrument.forex.currency_pair;
  * Доменная модель валютной пары.
  */
 public record CurrencyPair(
+
         String code1,
         String code2,
         String pair,
