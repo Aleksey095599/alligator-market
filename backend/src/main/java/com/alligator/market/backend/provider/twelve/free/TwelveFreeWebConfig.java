5def72cc989a76541b570e7363acb5badf378bb3
Алексей Б. <alexeybaranov86@gmail.com>
2025-07-10 13:08:21 +0300

    Update `TwelveFreeWebConfig`: inject `BuildProperties` to dynamically set `User-Agent` with app name and version

--- backend/src/main/java/com/alligator/market/backend/provider/twelve/free/TwelveFreeWebConfig.java
+++ backend/src/main/java/com/alligator/market/backend/provider/twelve/free/TwelveFreeWebConfig.java
@@ -1,6 +1,7 @@
 package com.alligator.market.backend.provider.twelve.free;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
@@ -16,25 +17,31 @@ public class TwelveFreeWebConfig {
 
     private final TwelveFreeProps props;
     private final HttpClient httpClient;
+    private final BuildProperties build;
 
     // Конструктор
     public TwelveFreeWebConfig(
             TwelveFreeProps props,
             @Qualifier("providerHttpClient") // Инжектируем общий для всех провайдеров http-клиент
-            HttpClient httpClient
+            HttpClient httpClient,
+            BuildProperties build
     )
     {
         this.props = props;
         this.httpClient = httpClient;
+        this.build = build;
     }
 
     // Строим бин web-клиента
     @Bean("twelveFreeWebClient")
     WebClient twelveFreeWebClient() {
+
+        String ua = build.getName() + "/" + build.getVersion();
+
         return WebClient.builder()
                 .baseUrl(props.baseUrl())
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .defaultHeader("User-Agent", "Alligator-Market/1.0")
+                .defaultHeader("User-Agent", ua)
                 .build();
     }
 }
