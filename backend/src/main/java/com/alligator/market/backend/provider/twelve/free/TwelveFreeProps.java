43e7177e87e6b64ee9d9ecaf0b71a46542070408
Алексей Б. <alexeybaranov86@gmail.com>
2025-07-10 12:02:20 +0300

    Remove `@ConfigurationProperty` annotations from `TwelveFreeProps` fields as they are unnecessary.

--- backend/src/main/java/com/alligator/market/backend/provider/twelve/free/TwelveFreeProps.java
+++ backend/src/main/java/com/alligator/market/backend/provider/twelve/free/TwelveFreeProps.java
@@ -1,7 +1,6 @@
 package com.alligator.market.backend.provider.twelve.free;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationProperty;
 
 /**
  * Параметры подключения к провайдеру TwelveData (free plan).
@@ -9,9 +8,6 @@ import org.springframework.boot.context.properties.ConfigurationProperty;
 @ConfigurationProperties("provider.connection-config.twelve-free")
 public record TwelveFreeProps (
 
-        @ConfigurationProperty("base-url")
         String baseUrl,
-
-        @ConfigurationProperty("api-key")
         String apiKey
 ) {}
