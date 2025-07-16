b299a33d5131ee7256df51d1c9ea9bbffaa68125
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-07-16 23:21:14 +0300

    refactor(domain): update comments in InstrumentType enum

--- domain/src/main/java/com/alligator/market/domain/instrument/InstrumentType.java
+++ domain/src/main/java/com/alligator/market/domain/instrument/InstrumentType.java
@@ -8,15 +8,6 @@ public enum InstrumentType {
     // Валютная пара на рынке Forex
     CURRENCY_PAIR,
 
-    // Акция компании
-    STOCK,
-
-    // Долговая ценная бумага
-    BOND,
-
-    // Фондовый индекс
-    INDEX,
-
-    // Национальная валюта
-    CURRENCY
+    // Валютный своп
+    CURRENCY_SWAP,
 }
