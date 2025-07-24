6d2cd65dc4a75a0763038fb2cb34a2307b2c7450
Алексей Б. <alexeybaranov86@gmail.com>
2025-07-24 18:17:29 +0300

    Rename and update constants in `ProviderProfileStatus` enum; refine Javadoc comments for clarity

--- domain/src/main/java/com/alligator/market/domain/provider/ProviderProfileStatus.java
+++ domain/src/main/java/com/alligator/market/domain/provider/ProviderProfileStatus.java
@@ -1,14 +1,12 @@
 package com.alligator.market.domain.provider;
 
 /**
- * Статус адаптера в профиле провайдера.
+ * Статус профиля провайдера.
 */
 public enum ProviderProfileStatus {
 
-    // Бин адаптера провайдера доступен в контексте Spring
-    AVAILABLE,
-
-    // Архивная запись (бин адаптера провайдера не был найден или был изменен)
-    ARCHIVED
+    ACTIVE,
+    REPLACED,
+    MISSING
 }
 
