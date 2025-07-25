4c690ea0f84c44cdf9fea7916a9927148299dfce
Aleksey095599 <alexeybaranov86@gmail.com>
2025-07-25 04:24:02 +0300

    Add ProviderProfile repository interface and adapter

---
+++ backend/src/main/java/com/alligator/market/backend/provider/profile/repository/ProviderProfileRepositoryAdapter.java
@@ -0,0 +1,26 @@
+package com.alligator.market.backend.provider.profile.repository;
+
+import com.alligator.market.backend.provider.profile.mapper.ProviderProfileMapper;
+import com.alligator.market.domain.provider.ProviderProfile;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Адаптер реализующий доменный репозиторий {@link com.alligator.market.domain.provider.ProviderProfileRepository} в контексте Spring Data JPA.
+ */
+@Repository
+@RequiredArgsConstructor
+public class ProviderProfileRepositoryAdapter implements com.alligator.market.domain.provider.ProviderProfileRepository {
+
+    private final ProviderProfileRepository jpaRepository;
+
+    @Override
+    public List<ProviderProfile> findAll() {
+        return jpaRepository.findAll(Sort.by("providerCode")).stream()
+                .map(ProviderProfileMapper::toDomain)
+                .toList();
+    }
+}
