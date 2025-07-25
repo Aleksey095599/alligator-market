bc97ce331dfd30610fe36d3f4ff68a6e63eaa46d
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-07-25 04:44:05 +0300

    refactor(backend): update import path in test file

--- backend/src/test/java/com/alligator/market/backend/provider/profile/service/ProviderContextScannerTest.java
+++ backend/src/test/java/com/alligator/market/backend/provider/profile/service/ProviderContextScannerTest.java
@@ -1,6 +1,6 @@
 package com.alligator.market.backend.provider.profile.service;
 
-import com.alligator.market.backend.provider.profile.sync.ProviderContextScanner;
+import com.alligator.market.backend.provider.profile.context_sync.ProviderContextScanner;
 import com.alligator.market.domain.provider.profile.ProviderProfile;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
