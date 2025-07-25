02373534aeebf6e18ad39ecb0346e7e06a0a26ba
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-07-25 04:43:45 +0300

    feat(backend): add DiffModel record for context sync

---
+++ backend/src/main/java/com/alligator/market/backend/provider/profile/context_sync/DiffModel.java
@@ -0,0 +1,13 @@
+package com.alligator.market.backend.provider.profile.context_sync;
+
+import com.alligator.market.backend.provider.profile.entity.ProviderProfileEntity;
+
+import java.util.List;
+
+public record DiffModel (
+
+        List<ProviderProfileEntity> toActive,
+        List<ProviderProfileEntity> toReplaced,
+        List<ProviderProfileEntity> toMissing
+
+) {}
