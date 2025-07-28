79499f0ab1da69370f02212d76f63ae87c756d5d
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-07-28 09:11:15 +0300

    refactor(backend): rename ProviderProfilesMatching to ContextDiffService

--- backend/src/main/java/com/alligator/market/backend/provider/profile/context_sync/ContextDiffService.java
+++ backend/src/main/java/com/alligator/market/backend/provider/profile/context_sync/ContextDiffService.java
@@ -15,7 +15,7 @@ import java.util.Map;
  */
 @Component
 @RequiredArgsConstructor
-public class ProviderProfilesMatching {
+public class ContextDiffService {
 
     private final ProviderContextScanner contextScanner;
     private final ProviderProfileService profileService;
