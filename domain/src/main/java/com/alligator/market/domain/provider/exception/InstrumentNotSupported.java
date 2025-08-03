f04bab202bb9b951afc12c0796cc75daa9684983
Aleksey095599 <alexeybaranov86@gmail.com>
2025-08-04 00:26:42 +0300

    Добавлено исключение InstrumentNotSupported и обновлен MarketDataProvider

---
+++ domain/src/main/java/com/alligator/market/domain/provider/exception/InstrumentNotSupported.java
@@ -0,0 +1,12 @@
+package com.alligator.market.domain.provider.exception;
+
+import com.alligator.market.domain.instrument.InstrumentType;
+
+/**
+ * Неподдерживаемый тип инструмента.
+ */
+public class InstrumentNotSupported extends RuntimeException {
+    public InstrumentNotSupported(InstrumentType type, String providerCode) {
+        super("Instrument type %s not supported by %s".formatted(type, providerCode));
+    }
+}
