b9fbfa377f886a24d4142ac0c9f34dad74c0a12e
Алексей Б. <alexeybaranov86@gmail.com>
2025-08-07 14:42:45 +0300

    Refine Javadocs in `InstrumentFeed` to clarify `priority` description.

--- domain/src/main/java/com/alligator/market/domain/quote/instrument_feed/InstrumentFeed.java
+++ domain/src/main/java/com/alligator/market/domain/quote/instrument_feed/InstrumentFeed.java
@@ -3,14 +3,12 @@ package com.alligator.market.domain.quote.instrument_feed;
 import com.alligator.market.domain.instrument.Instrument;
 import com.alligator.market.domain.provider.MarketDataProvider;
 
-import java.util.Objects;
-
 /**
  * Связка инструмента и провайдера.
  *
  * @param instrument финансовый инструмент {@link Instrument}
  * @param provider   провайдер рыночных данных {@link MarketDataProvider}
- * @param priority   приоритет провайдера (самый высокий =1)
+ * @param priority   приоритет провайдера (1,2,3...)
  */
 public record InstrumentFeed(
 
