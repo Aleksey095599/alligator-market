c868895476701fa5ceea6f1a4b9d80ef6d084791
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-08-17 20:45:08 +0300

    refactor(jpa): add BaseEntityMappingConfig for consistent field mappings

---
+++ backend/src/main/java/com/alligator/market/backend/common/jpa/BaseEntityMappingConfig.java
@@ -0,0 +1,18 @@
+package com.alligator.market.backend.common.jpa;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingInheritanceStrategy;
+import org.mapstruct.MappingTarget;
+
+@MapperConfig(mappingInheritanceStrategy = MappingInheritanceStrategy.AUTO_INHERIT_ALL_FROM_CONFIG)
+public interface BaseEntityMappingConfig {
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "createdTimestamp", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "createdVia", ignore = true)
+    @Mapping(target = "updatedTimestamp", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
+    @Mapping(target = "updatedVia", ignore = true)
+    void ignoreBaseFields(BaseEntity src, @MappingTarget BaseEntity dst);
+}
