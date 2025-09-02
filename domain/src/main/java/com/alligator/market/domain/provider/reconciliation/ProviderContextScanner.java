008ef498db01954b9b7cda728ed98fd5a8645ab7
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-09-03 01:16:04 +0300

    refactor(provider-reconciliation): add handlers retrieval to scanner
    Why: Introduced retrieval of instrument handlers to `ProviderContextScanner` to expand its functionality and support future reconciliation features.

--- domain/src/main/java/com/alligator/market/domain/provider/reconciliation/ProviderContextScanner.java
+++ domain/src/main/java/com/alligator/market/domain/provider/reconciliation/ProviderContextScanner.java
@@ -1,5 +1,6 @@
 package com.alligator.market.domain.provider.reconciliation;
 
+import com.alligator.market.domain.provider.handler.contract.InstrumentHandler;
 import com.alligator.market.domain.provider.profile.model.Profile;
 
 import java.util.Map;
@@ -10,7 +11,14 @@ import java.util.Map;
 public interface ProviderContextScanner {
 
     /**
-     * Вернуть карту профилей из контекста, где ключ — код провайдера.
+     * Вернуть карту профилей из контекста,
+     * где ключ — код провайдера.
      */
     Map<String, Profile> getProfiles();
+
+    /**
+     * Вернуть обработчики (handlers) для всех финансовых инструментов,
+     * где ключ — код провайдера.
+     * */
+    Map<String, InstrumentHandler> getHandlers();
 }
