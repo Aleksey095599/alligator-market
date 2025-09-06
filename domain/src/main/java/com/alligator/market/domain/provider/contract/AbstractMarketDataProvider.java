d564c7490e8617ca622fa4f1a8e23460f8c172d9
Aleksey095599 <alexeybaranov86@gmail.com>
2025-09-06 13:47:41 +0300

    Перевод контрактов на Publisher

--- domain/src/main/java/com/alligator/market/domain/provider/contract/AbstractMarketDataProvider.java
+++ domain/src/main/java/com/alligator/market/domain/provider/contract/AbstractMarketDataProvider.java
@@ -1,6 +1,11 @@
 package com.alligator.market.domain.provider.contract;
 
+import com.alligator.market.domain.instrument.base.Instrument;
+import com.alligator.market.domain.instrument.type.InstrumentType;
+import com.alligator.market.domain.provider.exception.InstrumentNotSupportedException;
 import com.alligator.market.domain.provider.handler.contract.InstrumentHandler;
+import com.alligator.market.domain.quote.QuoteTick;
+import org.reactivestreams.Publisher;
 import java.util.Collections;
 import java.util.Set;
 
@@ -19,6 +24,19 @@ public abstract class AbstractMarketDataProvider implements MarketDataProvider {
      */
     protected AbstractMarketDataProvider(Set<InstrumentHandler> handlers) {
         this.handlers = handlers;
+    }
+
+    /** Возвращает котировку. */
+    @Override
+    public Publisher<QuoteTick> getQuote(Instrument instrument) {
+        InstrumentType type = instrument.getType();
+        InstrumentHandler handler = findHandlerForInstrument(type);
+        if (handler == null) {
+            return subscriber -> subscriber.onError(
+                    new InstrumentNotSupportedException(type, getProfile().providerCode())
+            );
+        }
+        return handler.getInstrumentQuote(instrument);
     }
 
     /** Возвращает набор обработчиков. */
