1364fd9b7d7da6d41d037155ee2618b7e25a653d
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-09-06 14:53:34 +0300

    refactor(provider): update Javadoc for exception handling
    Why: Enhanced clarity by specifying `InstrumentNotSupportedException` in comments, improving documentation consistency.

--- domain/src/main/java/com/alligator/market/domain/provider/contract/AbstractMarketDataProvider.java
+++ domain/src/main/java/com/alligator/market/domain/provider/contract/AbstractMarketDataProvider.java
@@ -4,10 +4,10 @@ import com.alligator.market.domain.instrument.base.Instrument;
 import com.alligator.market.domain.instrument.type.InstrumentType;
 import com.alligator.market.domain.provider.exception.InstrumentNotSupportedException;
 import com.alligator.market.domain.provider.handler.contract.InstrumentHandler;
+import com.alligator.market.domain.provider.profile.model.Profile;
 import com.alligator.market.domain.quote.QuoteTick;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
-import java.util.Collections;
 import java.util.Set;
 
 /**
@@ -15,16 +15,37 @@ import java.util.Set;
  */
 public abstract class AbstractMarketDataProvider implements MarketDataProvider {
 
+    // Профиль провайдера
+    protected final Profile profile;
+
     // Набор обработчиков инструментов
     protected final Set<InstrumentHandler> handlers;
 
     /**
      * Конструктор базового провайдера.
      *
+     * @param profile  профиль провайдера
      * @param handlers набор обработчиков
      */
-    protected AbstractMarketDataProvider(Set<InstrumentHandler> handlers) {
-        this.handlers = handlers;
+    protected AbstractMarketDataProvider(Set<InstrumentHandler> handlers, Profile profile) {
+        this.profile = profile;
+        this.handlers = Set.copyOf(handlers);
+    }
+
+    /**
+     * Возвращает профиль провайдера.
+     */
+    @Override
+    public Profile getProfile() {
+        return profile;
+    }
+
+    /**
+     * Возвращает набор обработчиков.
+     */
+    @Override
+    public Set<InstrumentHandler> getHandlers() {
+        return handlers;
     }
 
     /**
@@ -40,12 +61,6 @@ public abstract class AbstractMarketDataProvider implements MarketDataProvider {
             return Flux.error(new InstrumentNotSupportedException(type, getProfile().providerCode()));
         }
         return handler.getInstrumentQuote(instrument);
-    }
-
-    /** Возвращает набор обработчиков. */
-    @Override
-    public Set<InstrumentHandler> getHandlers() {
-        return Collections.unmodifiableSet(handlers);
     }
 
     /**
