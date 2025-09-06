12a3a87f2fd28ffc6db155bd1b7a50a023c172fb
Aleksey095599 <alexeybaranov86@gmail.com>
2025-09-06 14:20:46 +0300

    Перенесён поиск обработчика в абстрактный провайдер

--- domain/src/main/java/com/alligator/market/domain/provider/contract/MarketDataProvider.java
+++ domain/src/main/java/com/alligator/market/domain/provider/contract/MarketDataProvider.java
@@ -5,7 +5,6 @@ import com.alligator.market.domain.provider.exception.InstrumentNotSupportedExce
 import com.alligator.market.domain.provider.handler.contract.InstrumentHandler;
 import com.alligator.market.domain.provider.profile.model.Profile;
 import com.alligator.market.domain.quote.QuoteTick;
-import com.alligator.market.domain.instrument.type.InstrumentType;
 import java.util.Set;
 
 import org.reactivestreams.Publisher;
@@ -28,17 +27,4 @@ public interface MarketDataProvider {
      */
     Publisher<QuoteTick> getQuote(Instrument instrument) throws InstrumentNotSupportedException;
 
-    /**
-     * Находит обработчик для указанного типа инструмента.
-     *
-     * @return подходящий обработчик или null
-     */
-    default InstrumentHandler findHandlerForInstrument(InstrumentType type) {
-        for (InstrumentHandler h : getHandlers()) {
-            if (h.getSupportedInstrumentType() == type) {
-                return h;
-            }
-        }
-        return null;
-    }
 }
