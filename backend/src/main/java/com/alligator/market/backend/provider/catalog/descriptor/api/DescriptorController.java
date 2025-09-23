306f2325b7773332c93e41d7e84c4e6eb5c5e5f2
Aleksey <alexeybaranov86@gmail.com>
2025-09-23 12:04:06 +0300

    Переименование ProfileController в DescriptorController для уточнения назначения класса

--- backend/src/main/java/com/alligator/market/backend/provider/catalog/descriptor/api/DescriptorController.java
+++ backend/src/main/java/com/alligator/market/backend/provider/catalog/descriptor/api/DescriptorController.java
@@ -1,17 +1,10 @@
 package com.alligator.market.backend.provider.catalog.descriptor.api;
 
-import com.alligator.market.backend.common.web.ApiResponse;
-import com.alligator.market.backend.common.web.ResponseEntityFactory;
-import com.alligator.market.backend.provider.catalog.descriptor.api.dto.DescriptorDto;
 import com.alligator.market.backend.provider.catalog.descriptor.service.DescriptorUseCase;
 import com.alligator.market.backend.provider.catalog.descriptor.api.dto.DescriptorDtoMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * REST-контроллер дескрипторов.
@@ -19,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/providers")
 @RequiredArgsConstructor
-public class ProfileController {
+public class DescriptorController {
 
     private final DescriptorUseCase service;
     private final DescriptorDtoMapper mapper;
