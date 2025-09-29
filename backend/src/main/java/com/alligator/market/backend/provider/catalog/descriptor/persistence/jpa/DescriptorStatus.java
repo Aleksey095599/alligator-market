c89c66058a48fd5c057a64f2aa82aa23aa7cbdb0
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-09-29 22:03:23 +0300

    refactor(descriptor): enhance Javadoc for status reconciliation

--- backend/src/main/java/com/alligator/market/backend/provider/catalog/descriptor/persistence/jpa/DescriptorStatus.java
+++ backend/src/main/java/com/alligator/market/backend/provider/catalog/descriptor/persistence/jpa/DescriptorStatus.java
@@ -2,6 +2,7 @@ package com.alligator.market.backend.provider.catalog.descriptor.persistence.jpa
 
 /**
  * Возможные статусы дескриптора провайдера.
+ * Статусы меняются в результате работы процесса реконсиляции.
  */
 public enum DescriptorStatus {
     ACTIVE,
