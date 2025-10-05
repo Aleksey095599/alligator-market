4002bcd3617cfd7bfdd147415dab39174d077462
Aleksey <alexeybaranov86@gmail.com>
2025-10-05 20:26:26 +0300

    не работает AI комментарий

--- domain/src/main/java/com/alligator/market/domain/instrument/type/InstrumentType.java
+++ domain/src/main/java/com/alligator/market/domain/instrument/type/InstrumentType.java
@@ -4,6 +4,6 @@ package com.alligator.market.domain.instrument.type;
  * Типы финансовых инструментов.
  */
 public enum InstrumentType {
-    FX_SPOT, // Валютная сделка спот
-    FX_SWAP  // Валютный своп
+    FX_SPOT,
+    FX_SWAP
 }
