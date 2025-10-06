abd7dbc1b6e236737b2048d0bfe995de33b333e5
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-10-07 01:06:29 +0300

    refactor(dto): clarify Javadoc in FxSpotListItemDto
    Why: Updated Javadoc to reflect the intended usage of the DTO, clarifying that it is for extracting data from the domain and sending it to the frontend, making validations unnecessary.

--- backend/src/main/java/com/alligator/market/backend/instrument/type/forex/spot/catalog/web/dto/FxSpotListItemDto.java
+++ backend/src/main/java/com/alligator/market/backend/instrument/type/forex/spot/catalog/web/dto/FxSpotListItemDto.java
@@ -6,7 +6,7 @@ import com.alligator.market.domain.instrument.type.forex.spot.model.ValueDateCod
  * DTO для передачи списка инструментов FX_SPOT с кодом и символом.
  */
 public record FxSpotListItemDto(
-        // Применяется только для передачи данных вовне, проверки не нужны
+        // Применяется только для получения данных из доменной модели и передачи во frontend => проверки не обязательны
         String code,
         String symbol,
         String baseCurrency,
