46db132eb50257dc214dbe5c42951dd146fd7378
Aleksey <alexeybaranov86@gmail.com>
2025-11-07 11:38:44 +0300

    не работает

--- backend/src/main/java/com/alligator/market/backend/common/web/http/ErrorCode.java
+++ backend/src/main/java/com/alligator/market/backend/common/web/http/ErrorCode.java
@@ -4,14 +4,14 @@ package com.alligator.market.backend.common.web.http;
  * Перечень стабильных кодов ошибок для фронта.
  */
 public enum ErrorCode {
-    /* Общие ошибки */
+    /* Общие ошибки: */
     UNEXPECTED_ERROR,
     MALFORMED_JSON,
     BAD_ARGUMENT,
     VALIDATION_ERROR,
     DATA_INTEGRITY_VIOLATION,
 
-    /* Провайдеры */
+    /* Провайдеры: */
     PROVIDER_CODE_DUPLICATE,
     PROVIDER_DISPLAY_NAME_DUPLICATE,
     HANDLER_NOT_FOUND,
@@ -19,13 +19,13 @@ public enum ErrorCode {
     INSTRUMENT_WRONG_CLASS,
     INSTRUMENT_WRONG_TYPE,
 
-    /* Валюты */
+    /* Валюты: */
     CURRENCY_ALREADY_EXISTS,
     CURRENCY_NAME_DUPLICATE,
     CURRENCY_NOT_FOUND,
     CURRENCY_USED_IN_FX_SPOT,
 
-    /* FX Spot */
+    /* FX_SPOT инструмент: */
     FX_SPOT_ALREADY_EXISTS,
     FX_SPOT_NOT_FOUND,
     FX_SPOT_SAME_CURRENCIES
