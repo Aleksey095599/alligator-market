20e733f4810106ab487db037e99b26d29e17aa6a
Aleksey Baranov <alexeybaranov86@gmail.com>
2025-11-29 17:32:30 +0300

    modify JavaDoc

--- backend/src/main/java/com/alligator/market/backend/provider/adapter/moex/iss/config/MoexIssAdapterProps.java
+++ backend/src/main/java/com/alligator/market/backend/provider/adapter/moex/iss/config/MoexIssAdapterProps.java
@@ -5,8 +5,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 /**
- * Параметры подключения к провайдеру рыночных данных MOEX ISS.
- * Автоматически считываются из настроек приложения.
+ * <b>Параметры подключения к провайдеру рыночных данных MOEX ISS.</b>
+ *
+ * <p>Данные параметры автоматически считываются из файла настроек приложения.</p>
+ * <p>Spring создаёт бин этого типа, который может быть внедрён в другие компоненты.</p>
  */
 @Validated
 @ConfigurationProperties("provider.connection-config.moex.iss")
