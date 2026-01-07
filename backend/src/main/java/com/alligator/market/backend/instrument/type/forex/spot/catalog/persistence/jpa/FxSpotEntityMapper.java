2eb3f9a888a5434d240e7c14c5fdcaa1d1371a0f
Aleksey Baranov <alexeybaranov86@gmail.com>
2026-01-07 14:39:00 +0300

    feat(fxspot): add domain mapping to FxSpotEntityMapper
    Body:
    Introduced `toDomain` method in `FxSpotEntityMapper` to map JPA entities to domain models:
    - Ensures non-null validation for `defaultQuoteFractionDigits` as per business logic.
    - Utilizes `CurrencyEntityMapper` for currency transformations.
    
    This enhancement enriches the mapper’s functionality, aligning it with domain requirements.

--- backend/src/main/java/com/alligator/market/backend/instrument/type/forex/spot/catalog/persistence/jpa/FxSpotEntityMapper.java
+++ backend/src/main/java/com/alligator/market/backend/instrument/type/forex/spot/catalog/persistence/jpa/FxSpotEntityMapper.java
@@ -1,5 +1,10 @@
 package com.alligator.market.backend.instrument.type.forex.spot.catalog.persistence.jpa;
 
+import com.alligator.market.backend.instrument.type.forex.currency.catalog.persistence.jpa.CurrencyEntityMapper;
+import com.alligator.market.domain.instrument.type.forex.spot.model.FxSpot;
+
+import java.util.Objects;
+
 /**
  * Маппер JPA-сущности в доменную модель FX_SPOT.
  */
@@ -12,5 +17,23 @@ public final class FxSpotEntityMapper {
         throw new UnsupportedOperationException("Utility class");
     }
 
+    /**
+     * Преобразует JPA-сущность в доменную модель: {@link FxSpotEntity} --> {@link FxSpot}
+     */
+    public static FxSpot toDomain(FxSpotEntity e) {
+        Objects.requireNonNull(e, "entity must not be null");
 
+        // Поле defaultQuoteFractionDigits не является идентичностью сущности, поэтому может быть null.
+        // Однако по бизнес логике доменная модель не предполагает возможность null => нужна null проверка.
+        int digits = Objects.requireNonNull(e.getDefaultQuoteFractionDigits(),
+                "defaultQuoteFractionDigits must not be null");
+
+        // Собираем и возвращаем доменную модель
+        return new FxSpot(
+                CurrencyEntityMapper.toDomain(e.getBaseCurrency()),
+                CurrencyEntityMapper.toDomain(e.getQuoteCurrency()),
+                e.getTenor(),
+                digits
+        );
+    }
 }
