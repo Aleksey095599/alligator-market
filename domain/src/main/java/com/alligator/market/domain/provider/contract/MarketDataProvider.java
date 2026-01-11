a518f8b984e2e9d47e105f4ccc93f0a4b11f2eae
Aleksey Baranov <alexeybaranov86@gmail.com>
2026-01-12 02:08:28 +0300

    refactor(javadoc): extend MarketDataProvider description

--- domain/src/main/java/com/alligator/market/domain/provider/contract/MarketDataProvider.java
+++ domain/src/main/java/com/alligator/market/domain/provider/contract/MarketDataProvider.java
@@ -1,12 +1,16 @@
 package com.alligator.market.domain.provider.contract;
 
+import com.alligator.market.domain.instrument.code.InstrumentCode;
 import com.alligator.market.domain.instrument.contract.Instrument;
+import com.alligator.market.domain.instrument.type.InstrumentType;
 import com.alligator.market.domain.provider.code.ProviderCode;
 import com.alligator.market.domain.provider.contract.passport.ProviderPassport;
 import com.alligator.market.domain.provider.contract.policy.ProviderPolicy;
 import com.alligator.market.domain.provider.contract.settings.ProviderSettings;
 import com.alligator.market.domain.quote.tick.model.QuoteTick;
 import org.reactivestreams.Publisher;
+
+import java.util.Set;
 
 /**
  * Контракт провайдера рыночных данных.
@@ -33,6 +37,18 @@ public sealed interface MarketDataProvider permits AbstractMarketDataProvider {
      */
     @SuppressWarnings("unused")
     ProviderSettings settings(); // <-- Точка роста, без реализации (заглушка)
+
+    /**
+     * Иммутабельный набор кодов поддерживаемых инструментов.
+     */
+    @SuppressWarnings("unused")
+    Set<InstrumentCode> instrumentsCodes();
+
+    /**
+     * Иммутабельный набор типов поддерживаемых инструментов.
+     */
+    @SuppressWarnings("unused")
+    Set<InstrumentType> instrumentsTypes();
 
     /**
      * Унифицированная операция получения котировок для финансового инструмента.
