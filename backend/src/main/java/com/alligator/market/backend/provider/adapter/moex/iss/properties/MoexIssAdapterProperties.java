7af26e4cf8f932379a3c6ed98f10a2593f19cd4e
Aleksey Baranov <alexeybaranov86@gmail.com>
2026-02-08 00:24:50 +0300

    refactor(backend): simplify MoexIssAdapterProperties Javadoc

--- backend/src/main/java/com/alligator/market/backend/provider/adapter/moex/iss/properties/MoexIssAdapterProperties.java
+++ backend/src/main/java/com/alligator/market/backend/provider/adapter/moex/iss/properties/MoexIssAdapterProperties.java
@@ -7,13 +7,9 @@ import org.springframework.validation.annotation.Validated;
 /**
  * Параметры подключения к провайдеру рыночных данных MOEX ISS.
  *
- * <p>Данные параметры автоматически считываются из файла настроек приложения. Spring создаёт бин этого типа,
- * который можно внедрять в другие компоненты.</p>
+ * <p>Параметры автоматически считываются из файла настроек приложения.</p>
  */
 @Validated
 @ConfigurationProperties("provider.connection-config.moex.iss")
-public record MoexIssAdapterProperties(
-        @NotBlank
-        String baseUrl
-) {
+public record MoexIssAdapterProperties(@NotBlank String baseUrl) {
 }
