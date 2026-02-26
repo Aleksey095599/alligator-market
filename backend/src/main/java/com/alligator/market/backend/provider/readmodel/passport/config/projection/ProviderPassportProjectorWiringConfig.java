ca70c9d3fef69b61af74ea1c50027a2c79402755
Aleksey Baranov <alexeybaranov86@gmail.com>
2026-02-26 07:56:14 +0300

    feat(backend/provider): add imports in `ProviderPassportProjectorWiringConfig`
    Enable streamlined configuration by importing dependent wiring classes for `ProviderRegistry` and `ProviderPassportProjectionWriteStore`.

--- backend/src/main/java/com/alligator/market/backend/provider/readmodel/passport/config/projection/ProviderPassportProjectorWiringConfig.java
+++ backend/src/main/java/com/alligator/market/backend/provider/readmodel/passport/config/projection/ProviderPassportProjectorWiringConfig.java
@@ -1,15 +1,22 @@
 package com.alligator.market.backend.provider.readmodel.passport.config.projection;
 
+import com.alligator.market.backend.provider.readmodel.passport.config.store.ProviderPassportProjectionWriteStoreWiringConfig;
+import com.alligator.market.backend.provider.registry.wiring.ProviderRegistryWiringConfig;
 import com.alligator.market.domain.provider.readmodel.passport.projection.ProviderPassportProjector;
 import com.alligator.market.domain.provider.readmodel.passport.store.ProviderPassportProjectionWriteStore;
 import com.alligator.market.domain.provider.registry.ProviderRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * Конфигурация wiring {@link ProviderPassportProjector}.
  */
 @Configuration(proxyBeanMethods = false)
+@Import({
+        ProviderRegistryWiringConfig.class,
+        ProviderPassportProjectionWriteStoreWiringConfig.class,
+})
 public class ProviderPassportProjectorWiringConfig {
 
     public static final String BEAN_PROVIDER_PASSPORT_PROJECTOR = "providerPassportProjector";
