4e658cb147f6cbcb4417bdc1f77a2abc3206da49
Aleksey095599 <alexeybaranov86@gmail.com>
2026-03-07 22:56:56 +0300

    Consolidate FX spot migrations into single schema

--- backend/src/main/resources/db/migration/instrument/asset/forex/contract/spot/V20260306_01__extend_fx_spot_tenor_storage.sql
+++
@@ -1,6 +0,0 @@
--- Ослабляем ограничение tenor: храним код как строку без фиксированного перечня.
-ALTER TABLE instrument_fx_spot
-    DROP CONSTRAINT IF EXISTS chk_fx_spot_tenor_allowed;
-
-ALTER TABLE instrument_fx_spot
-    ALTER COLUMN tenor TYPE VARCHAR(5);
