0c4f77c83018672f1e9b41edf571175752e10e57
Aleksey Baranov <alexeybaranov86@gmail.com>
2026-05-04 14:33:29 +0300

    feat(domain): add AnalyticalTwapLastPriceCollectionProcess
    Introduce `AnalyticalTwapLastPriceCollectionProcess` implementing `MarketDataCollectionProcess` to support analytical TWAP last price processing. Update `spelling.dic` to include "Twap".

---
+++ domain/src/main/java/com/alligator/market/domain/marketdata/collection/process/twap/analytical/AnalyticalTwapLastPriceCollectionProcess.java
@@ -0,0 +1,6 @@
+package com.alligator.market.domain.marketdata.collection.process.twap.analytical;
+
+import com.alligator.market.domain.marketdata.collection.process.MarketDataCollectionProcess;
+
+public class AnalyticalTwapLastPriceCollectionProcess implements MarketDataCollectionProcess {
+}
