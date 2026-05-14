9e944ea7edaabcf0794e4c61992fdb63b71a3117
Aleksey Baranov <alexeybaranov86@gmail.com>
2026-05-14 18:00:20 +0300

    remove redundant comment from SourcePlanEntryLifecycleStatus enum

--- domain/src/main/java/com/alligator/market/domain/sourceplan/SourcePlanEntryLifecycleStatus.java
+++ domain/src/main/java/com/alligator/market/domain/sourceplan/SourcePlanEntryLifecycleStatus.java
@@ -2,9 +2,6 @@ package com.alligator.market.domain.sourceplan;
 
 import com.alligator.market.domain.shared.code.DomainCodeFormat;
 
-/**
- * RETIRED is reversible: sync can reactivate an entry when its referenced passports become active again.
- */
 public enum SourcePlanEntryLifecycleStatus {
     ACTIVE,
     RETIRED;
